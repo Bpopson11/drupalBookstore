2230c6a691aa7e09b4c7e20ef7b88f5a224814a5
Bri <bpopson@gmail.com>
2016-04-30 21:04:02 -0700

    add functioning form for shift cypher with no output

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -213,15 +213,15 @@
  * @endcode
  */
 $databases = array (
-  'default' => 
+  'default' =>
   array (
-    'default' => 
+    'default' =>
     array (
       'database' => 'bookstore',
       'username' => 'BookMaster',
       'password' => 'books',
       'host' => '127.0.0.1',
-      'port' => '8889',
+      'port' => '3306',
       'driver' => 'mysql',
       'prefix' => '',
     ),

cc24ae02a0c6d0d4efab6901a6fdabea47c81c42
Bri Popson <bpopson@gmail.com>
2016-05-18 08:48:24 -0700

    correct ajax view to block and dna array to string error

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -221,7 +221,7 @@ $databases = array (
       'username' => 'BookMaster',
       'password' => 'books',
       'host' => '127.0.0.1',
-      'port' => '3306',
+      'port' => '8889',
       'driver' => 'mysql',
       'prefix' => '',
     ),
